83606e2becbae8c17febc3ed1e496aabd74d0b72
jthanawat <kla13thanawat@gmail.com>
2020-03-10 09:13:58 +0700

    add function setTimeout

---
+++ 7 - Node.js/Basic/Basic Node.js-Part 1/setTimeout.js
@@ -0,0 +1,19 @@
+// * Node.js *
+// Lab - 1
+// ใช้ setTimeout เพื่อให้ console.log(‘a’) เมื่อเวลาผ่านไป 1 วินาที
+// (ต่อจากข้อ 1) เมื่อ console.log(‘a’) เสร็จ ให้ทำการ console.log(‘b’) เมื่อเวลาผ่านไป 1 วินาที
+// (ต่อจากข้อ 2) เมื่อ console.log(‘b’) เสร็จ ให้ทำการ console.log(‘c’) เมื่อเวลาผ่านไป 1 วินาที
+// (ต่อจากข้อ 3) เมื่อ console.log(‘c’) เสร็จ ให้ทำการ console.log(‘d’) เมื่อเวลาผ่านไป 1 วินาที
+
+setTimeout(() => {
+  console.log('a');
+  setTimeout(() => {
+    console.log('b');
+    setTimeout(() => {
+      console.log('c');
+      setTimeout(() => {
+        console.log('d');
+      }, 1000)
+    }, 1000)
+  }, 1000)
+}, 1000)
