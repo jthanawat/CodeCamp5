0cf09d6244b0da377eac93bd917e029434df6d1e
jthanawat <kla13thanawat@gmail.com>
2020-03-10 09:13:40 +0700

    introduce node.js

---
+++ 7 - Node.js/Basic/Basic Node.js-Part 1/node.js
@@ -0,0 +1,33 @@
+// const fs = require('fs');
+
+// console.log(typeof fs);
+
+// fs.writeFileSync('CoDeCaMp.txt','Hello, World');
+
+// let content = fs.readFileSync('CoDeCaMp.txt', {
+//     encoding: 'utf-8'
+// });
+// console.log(content);
+
+
+// function mal(a, b, callbackFunction) {
+//     callbackFunction(a * b)
+// }
+
+// mal(2, 3,(result) => {
+//     console.log(result);
+// })
+
+
+// function max(a, b, callbackFunction) {
+//     if(a > b) {
+//         callbackFunction(a)
+//     } else {
+//         callbackFunction(b)
+//     }
+// }
+
+// max(2, 3,(result) => {
+//     console.log(result);
+// })
+
