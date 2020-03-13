0b845e818c6913640807c6bae810c862ebc5169b
jthanawat <kla13thanawat@gmail.com>
2020-03-13 09:52:17 +0700

    add comment

--- 7 - Node.js/Basic/Basic Node.js-Part 2/Day2/2_router.js
+++ 7 - Node.js/Basic/Basic Node.js-Part 2/Day2/2_router.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const catRoute = require('./routes/catRoute');
+const catRoute = require('./routes/catRoute'); // importing Express catRoute
 const dogRoute = require('./routes/dogRoute');
 const pictureRoute = require('./routes/pictureRoute');
 
@@ -11,3 +11,5 @@ app.use('/dog', dogRoute)
 app.listen(8000, function() {
   console.log('Server is running on port 8000');
 })
+
+// Handler is a callback function that executes when a matching request type is found on the relevant route
