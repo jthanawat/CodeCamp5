d236bb649c376587fcf4d4c34d9eaa8bbf83dc33
jthanawat <kla13thanawat@gmail.com>
2020-03-30 16:03:10 +0700

    Compare class and hook

--- 9 - React/Review/hook-by-example/src/App.js
+++ 9 - React/Review/hook-by-example/src/App.js
@@ -2,23 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+// class App extends React.Component {
+//   state = {
+//     name: "sonter",
+//     age: 19
+//   }
+    // set in class it will merge.
+//   setSecondState = () => {
+//     this.setState({
+//       age: 40
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <button onClick={this.setSecondState}>Change age to 40</button>
+//       </div>
+//     )
+//   }
+// }
+
+
+// hook will overlay and display only in setState "age : 40" not display name: sonter like class
 function App() {
+  const [state, setState] = useState ({
+    name: "sonter",
+    age: 19
+  })
+ 
+
+  function setSecondState() {
+    setState ({
+      age: 40
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={setSecondState}> Change Second State </button>
     </div>
   );
 }
