6cae4ad08acd5c0b21071ee961a4b743d8eb0398
jthanawat <kla13thanawat@gmail.com>
2020-06-30 01:20:49 +0700

    learn react form validator

---
+++ 9 - React/form/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import RegisterForm from './Container/RegisterForm/RegisterForm';
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
